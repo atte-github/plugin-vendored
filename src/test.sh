894d460e89b64f8041e1e8a73fc0eda9f760b3cb
stephanieatte <122297064+stephanieatte@users.noreply.github.com>
2023-06-13 14:48:44 -0600

    Update test.sh

--- src/test.sh
+++ src/test.sh
@@ -3,7 +3,9 @@ echo "the rety count is $BUILDKITE_RETRY_COUNT"
 if [ $BUILDKITE_RETRY_COUNT -gt 0 ]; then
    var="building"
    echo $var
+   export $var
 else 
    var="default"
    echo $var
+   export $var
 fi
